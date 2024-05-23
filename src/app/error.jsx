e28ac9d9f7706e93b6f1bdd910bb70f45c98b5ac
@Nagendra_Yaparala <yaparalanagendra@gmail.com>
2024-05-23 16:53:37 +0530

    after build changes

--- src/app/error.jsx
+++ src/app/error.jsx
@@ -1,16 +1,17 @@
 'use client'
-import React, { useEffect } from 'react'
+import React, { useEffect } from 'react';
 
-const error = ({error,reset}) => {
-    useEffect(()=>{
+const Error = ({ error, reset }) => {
+    useEffect(() => {
         console.log(error);
-    },[error])
-  return (
-    <div>
-        <h1>Something went Wrong.Please Try Again</h1>
-        <button onClick={()=>reset()}>Try Again</button>
-    </div>
-  )
-}
+    }, [error]);
 
-export default error
+    return (
+        <div>
+            <h1>Something went wrong. Please try again.</h1>
+            <button onClick={reset}>Try Again</button>
+        </div>
+    );
+};
+
+export default Error;
